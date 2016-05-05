813710207f8495932e681c20aa35996b403c496d
Dylan Richards <dylanerichards@gmail.com>
2016-05-04 22:08:14 -0700

    Clean up

--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -2,6 +2,7 @@ describe "Topics" do
   feature "home page" do
     scenario 'displays list of topics' do
       visit "/"
+
       expect(page).to have_content "Listing All Topics"
     end
 
@@ -19,17 +20,18 @@ describe "Topics" do
   feature "show page" do
     scenario 'shows an individual topic' do
       create_topic(name: "HTML", content: "foo")
+
       visit "/"
 
       click_on "HTML"
 
       expect(page).to have_content "HTML"
-      
       expect(page).to have_content "foo"
     end
 
     scenario "has the name of topic in the url" do
       create_topic(name: "HTML", content: "foo")
+
       visit "/topics/html"
 
       expect(page).to have_content "HTML"
@@ -45,9 +47,7 @@ describe "Topics" do
       visit root_path
       click_on "HTML"
 
-      expect {
-        click_on "delete"
-      }.to change(Topic, :count).by -1
+      expect { click_on "delete" }.to change(Topic, :count).by -1
     end
   end
 
@@ -56,9 +56,7 @@ describe "Topics" do
       create_topic(name: "HTML", content: "foo")
 
       visit "/topics/html"
-
       click_on "edit"
-
       fill_in "Name", with: "New Name"
 
       click_on "Update Topic"
@@ -72,8 +70,6 @@ private
 
 def create_topic(args = {})
   visit "/topics/new"
-
-  expect(page).to have_content "New Topic"
 
   fill_in "Name", with: args[:name]
   fill_in "Content", with: args[:content]
