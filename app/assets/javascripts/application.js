2b14947c78cabf4b25b270a27f61b91a64f52834
Ka Mok <heykamok@gmail.com>
2016-05-07 21:01:58 -0400

    Add tag in new topics form, but encountering bug with UI.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,9 @@
 //= require bootstrap-wysihtml5
 //= require_tree .
 
-$(document).on('page:load', function(){
-  window['rangy'].initialized = false
+$(document).ready(function() {
+
+  $('.wysihtml5').each(function(i, elem) {
+    $(elem).wysihtml5();
+  });
 });
