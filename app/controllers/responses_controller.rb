0fda2434b137fe3791b4d61e31dea32f5e80d6f0
Dylan Richards <dylanerichards@gmail.com>
2016-07-10 09:56:59 -0700

    Fix flash message

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -22,11 +22,9 @@ class ResponsesController < ApplicationController
       if @response.save
         ResponseMailer.response_email(params[:response][:full_name], params[:response][:body]).deliver_now
 
-        format.html { redirect_to root_path, notice: 'See you soon!' }
-        format.json { render :show, status: :created, location: @response }
+        format.html { redirect_to root_path, notice: "Thank you for getting in touch! We will get back to you within 24 hours." }
       else
         format.html { render :new }
-        format.json { render json: @response.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -35,10 +33,8 @@ class ResponsesController < ApplicationController
     respond_to do |format|
       if @response.update(response_params)
         format.html { redirect_to @response, notice: 'Response was successfully updated.' }
-        format.json { render :show, status: :ok, location: @response }
       else
         format.html { render :edit }
-        format.json { render json: @response.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -47,7 +43,6 @@ class ResponsesController < ApplicationController
     @response.destroy
     respond_to do |format|
       format.html { redirect_to responses_url, notice: 'Response was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
