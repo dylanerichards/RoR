d90c2f459fae6d7cae5fa38c27459d35dc78519d
Ka_Lenovo <heykamok@gmail.com>
2016-07-17 15:18:26 -0400

    Upon creating response, create a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < Clearance::UsersController
 	skip_before_filter  :verify_authenticity_token
 
 	def newsletter_signup
-		require 'pry'
-		binding.pry
 		user = User.new(email: params[:email], password: PasswordGenerator.generate).save
 		redirect_to root_path
 	end
