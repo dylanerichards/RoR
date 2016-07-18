ae91dea6533355c6b92a04aa21dc01968dd688d1
Dylan Richards <dylanerichards@gmail.com>
2016-07-17 21:30:55 -0700

    Fix indentation

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var firstTime = localStorage.getItem('visited');
 
   if (firstTime != '1') {
-		window.location = "#modal";
-		localStorage.setItem('visited', '1');
+    window.location = "#modal";
+    localStorage.setItem('visited', '1');
   }
 });
