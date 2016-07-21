7eb837ac4b1c5c0895fd6b44c1db8e67cc524eef
Richards <der@acbc3284b5e7.ant.amazon.com>
2016-07-21 10:36:25 -0700

    Remove SSL test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
 
-  before_filter :expire_hsts
-
   protect_from_forgery with: :null_session
-
-  def expire_hsts
-    response.headers["Strict-Transport-Security"] = 'max-age=0'
-  end
 end
