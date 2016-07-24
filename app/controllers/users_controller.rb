c7703b323fd147ff667176d4c9a0bc15497ad661
Richards <der@acbc3284b5e7.ant.amazon.com>
2016-07-23 21:54:15 -0700

    Remove empty line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < Clearance::UsersController
 
   before_filter :authenticate, only: :emails
 
-
   def newsletter_signup
     user = User.new(email: params[:email], password: PasswordGenerator.generate).save
     flash[:notice] = "Thanks for signing up!"
